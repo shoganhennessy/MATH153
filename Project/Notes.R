ca314cab23740e1413c98e1b10824148849d19cf
Senan Hogan-H <31498943+shoganhennessy@users.noreply.github.com>
2018-02-27 21:28:13 -0800

    Update Notes.R

--- Project/Notes.R
+++ Project/Notes.R
@@ -8,3 +8,6 @@ https://www.sciencedirect.com/science/article/pii/0304407694900779
 
 When inequalities diverge, distributional analysis of changing inequality
 MC Wolfson - The American Economic Review
+
+post worth mentioning
+http://econlog.econlib.org/archives/2009/11/why_arent_acade.html
