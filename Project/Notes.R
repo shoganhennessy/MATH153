20e9e705e2bc1dcc463a5f8befab5f3fd881107c
Senan Hogan-H <31498943+shoganhennessy@users.noreply.github.com>
2018-02-27 20:43:55 -0800

    Create Notes.R

---
+++ Project/Notes.R
@@ -0,0 +1,10 @@
+Wage data from Macrh CPS
+
+Question regular Mincer-type linear function.
+
+
+https://www.sciencedirect.com/science/article/pii/0304407694900779
+
+
+When inequalities diverge, distributional analysis of changing inequality
+MC Wolfson - The American Economic Review
