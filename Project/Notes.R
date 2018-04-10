8bc9b56d48c44dd9353c401f17152d784f2480f6
Senan Hogan-H <31498943+shoganhennessy@users.noreply.github.com>
2018-04-10 13:42:05 -0700

    Update Notes.R

--- Project/Notes.R
+++ Project/Notes.R
@@ -13,3 +13,6 @@ https://www.sciencedirect.com/science/article/pii/S027277571200026X
 
 post worth mentioning
 http://econlog.econlib.org/archives/2009/11/why_arent_acade.html
+
+
+https://rpubs.com/mfondoum/bayesian_linear_regression
